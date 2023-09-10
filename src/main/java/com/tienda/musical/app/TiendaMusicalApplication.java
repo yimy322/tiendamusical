516a11697ad4b968e178de21331f8dd81d7d164d
yimy322 <fh2001yj@gmail.com>
2023-09-10 11:06:45 -0500

    Version inicial del proyecto de tienda musical

---
+++ src/main/java/com/tienda/musical/app/TiendaMusicalApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.musical.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMusicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMusicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda/musical/app/TiendaMusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.musical.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMusicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
